9d089ab68212ad0243eac9b1a9d6ae2fca69353f
Marco Caberletti <marco.caberletti@gmail.com>
2017-05-02 16:08:17 +0200

    First import

---
+++ rpm/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -ex
+
+PLATFORM=${PLATFORM:-centos7}
+COMPONENTS=${COMPONENTS:-"iam-login-service"}
+
+pkg_base_image_name="italiangrid/pkg.base:${PLATFORM}"
+
+if [ -n "${USE_DOCKER_REGISTRY}" ]; then
+  pkg_base_image_name="${DOCKER_REGISTRY_HOST}/${pkg_base_image_name}"
+fi
+
+if [ -z ${MVN_REPO_CONTAINER_NAME+x} ]; then
+  mvn_repo_name=$(basename $(mktemp -u -t mvn-repo-XXXXX))
+  # Create mvn repo container
+  docker create -v /m2-repository --name ${mvn_repo_name} ${pkg_base_image_name}
+else
+  mvn_repo_name=${MVN_REPO_CONTAINER_NAME}
+fi
+
+# Run packaging
+for c in ${COMPONENTS}; do
+  build_env=""
+
+  build_repo_var="BUILD_REPO_$(echo ${c} | tr '[:lower:]' '[:upper:]' | tr '-' '_')"
+
+  while read -r line
+  do
+    build_env="${build_env} -e ${line}"
+  done < "$c/build-env"
+
+  if [ -n "${PKG_BUILD_NUMBER}" ]; then
+    build_env="${build_env} -e BUILD_NUMBER=${PKG_BUILD_NUMBER}"
+  fi
+
+  if [ -n "${PKG_PACKAGES_DIR}" ]; then
+    build_env="${build_env} -e PKG_PACKAGES_DIR=${PKG_PACKAGES_DIR}"
+  fi
+
+  if [ -n "${PKG_STAGE_DIR}" ]; then
+    build_env="${build_env} -e PKG_STAGE_DIR=${PKG_STAGE_DIR}"
+  fi
+
+  if [ -n "${!build_repo_var}" ]; then
+    build_env="${build_env} -e BUILD_REPO=${!build_repo_var}"
+  fi
+
+  if [ -n "${PKG_TAG}" ]; then
+    build_env="${build_env} -e PKG_TAG=${PKG_TAG}"
+  fi
+
+  if [ -n "${PKG_REPO}" ]; then
+    build_env="${build_env} -e PKG_REPO=${PKG_REPO}"
+  fi
+
+  if [ -n "${DATA_CONTAINER_NAME}" ]; then
+    volumes_conf="${volumes_conf} --volumes-from ${DATA_CONTAINER_NAME}"
+  fi
+
+  if [ -n "${STAGE_ALL}" ]; then
+      build_env="${build_env} -e PKG_STAGE_RPMS=1"
+  fi
+
+  if [ -n "${PKG_TAG}" ]; then
+    build_env="${build_env} -e PKG_TAG=${PKG_TAG}"
+  fi
+
+  docker run -i --volumes-from ${mvn_repo_name} \
+    ${volumes_conf} \
+    ${build_env} \
+    ${DOCKER_ARGS} \
+    ${pkg_base_image_name}
+done
---
+++ rpm/iam-login-service/iam-login-service.spec
@@ -0,0 +1,73 @@
+%define name            iam-login-service
+%define warversion      0.6.0
+%define user            iam
+
+%define jdk_version     1.8.0
+%define mvn_version     3.3.0
+
+Name:		%{name}
+Version:	%{warversion}
+Release:	1%{?dist}
+Summary:	INDIGO Identity and Access Management Service.
+
+Group:		Applications/Web
+License:	apache2
+URL:		https://github.com/indigo-dc/iam
+
+BuildArch: noarch
+BuildRequires: java-%{jdk_version}-openjdk-devel
+BuildRequires: maven >= %{mvn_version}
+
+Requires:	java-%{jdk_version}-openjdk
+
+%description
+The INDIGO IAM (Identity and Access Management service) provides 
+user identity and policy information to services so that consistent 
+authorization decisions can be enforced across distributed services.
+
+%prep
+
+%build
+sudo cp -r $HOME/sources/%{name} /
+sudo chown -R $(id -un):$(id -un) /%{name}
+cd /%{name}
+mvn -U clean package
+
+%install
+pwd
+cd ${RPM_BUILD_ROOT}
+mkdir -p var/lib/indigo/%{name}
+mkdir -p usr/lib/systemd/system
+mkdir -p etc/sysconfig
+cp /%{name}/%{name}/target/%{name}.war var/lib/indigo/%{name}
+cp /%{name}/rpm/SOURCES/%{name}.service usr/lib/systemd/system
+cp /%{name}/rpm/SOURCES/%{name} etc/sysconfig
+
+%clean
+
+%pre
+
+%post
+/usr/bin/id -u %{user} > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+  useradd --comment "INDIGO IAM" --system --user-group --home-dir /var/lib/indigo/%{name} --no-create-home --shell /sbin/nologin %{user}
+fi
+chown -R %{user}:%{user} /var/lib/indigo/%{name}
+systemctl daemon-reload
+
+%preun
+
+%postun
+systemctl daemon-reload
+
+%files
+%config(noreplace) /etc/sysconfig/iam-login-service
+%dir /var/lib/indigo
+%dir /var/lib/indigo/%{name}
+/var/lib/indigo/%{name}/%{name}.war
+/usr/lib/systemd/system/%{name}.service
+
+%changelog
+* Thu Apr 27 2017 Marco Caberletti <marco.caberletti@cnaf.infn.it> 0.6.0
+- Initial IAM Login Service for Indigo 2.
+
