4c54cd1938987bcf5a6ac5e849257b6f067e5dbe
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-10-17 16:42:14 +0200

    ...

--- upload-packages.sh
+++ upload-packages.sh
@@ -6,9 +6,16 @@ set -ex
 job_name=$(echo ${BUILD_TAG} | sed -e 's/^jenkins-//')
 
 for platform in ${RPM_PLATFORMS}; do
-  nexus-assets-flat-upload -u ${NX_USERNAME} \
-    -p ${NX_PASSWORD} \
-    -H ${NEXUS_HOST} \
-    -r ${TARGET_REPO}/${job_name}/${platform} \
-    -d artifacts/packages/${platform}/RPMS
+  dir_to_upload="artifacts/packages/${platform}/RPMS"
+  if [ ! -d "${dir_to_upload}" ]; then
+    echo "Directory to upload not found!"
+    ls -lR
+    exit 1
+  else
+    nexus-assets-flat-upload -u ${NX_USERNAME} \
+      -p ${NX_PASSWORD} \
+      -H ${NEXUS_HOST} \
+      -r ${TARGET_REPO}/${job_name}/${platform} \
+      -d ${dir_to_upload}
+  fi
 done
